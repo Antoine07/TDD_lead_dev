2ac77f52291ca3b032892b8d9910b7dfa2aaddd6
Antoine07 <antoine.lucsko@gmail.com>
2022-06-29 12:01:05 +0200

    user test

---
+++ chap1_introduction/Exercices/Exercice_03_Model/tests/UserTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Yaml\Parser;
+
+use App\{User, Model};
+
+class UserTest extends TestCase
+{
+
+  protected $model;
+
+  public function setUp(): void
+  {
+   
+   
+  }
+
+  /**
+   * @test count method insert
+   */
+  public function testSeedsCreate()
+  {
+  }
+
+  /**
+   * @test save method insert
+   */
+  public function testInsertSave()
+  {
+  
+  }
+
+  /**
+   * @test save method update
+   */
+  public function testUpdateSave()
+  {
+  }
+
+  /**
+   * @test delete resource by id
+   */
+  public function testDelete()
+  {
+
+  }
+}
